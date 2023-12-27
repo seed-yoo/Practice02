46d5974462ea1b7574d951cd8021bb33ab48709a
seed-yoo <lukdown10@naver.com>
2023-12-27 15:40:14 +0900

    연습문제

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,20 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		/*
+		 다음중 에러가 발생하는 문장은? 1번
+			(1) if( i=0 ) {….}				==> i == 0 으로 수정해야한다
+			(2) if( j<10 ) {…}
+			(3) switch( 'A' ){….}
+			(4) switch( 100 ){…}
+			(5) switch( "A" ){…}
+			(6) if( name == "홍길동" )
+
+		 */
+		
+	}
+
+}
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,25 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		/*
+		 다음중 switch 문에서 조건식에 넣을 수 있는 데이터 타입은? 2번, 3번, 4번, 5번, 9번
+			(1) boolean
+			(2) char
+			(3) byte
+			(4) short
+			(5) int
+			(6) long
+			(7) float
+			(8) double
+			(9) String
+		 */
+		
+
+		}
+		
+	}
+
+
